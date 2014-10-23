93f796c5bbbb885d04d4cbd8217583b058392914
Jens Pettersson <jens.pettersson@live.se>
2014-10-23 15:38:15 +0200

    Added word reverser

---
+++ FFCG.Reverser/FFCG.Reverser.Tests/WordReverserTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace FFCG.Reverser.Tests
+{
+    [TestFixture]
+    public class WordReverserTests
+    {
+        [Test]
+        public void Reverse_WithOneWord_ReturnsThatWord()
+        {
+            ArrangeActAssert("hello", "hello");
+        }
+
+        [Test]
+        public void Reverse_WithTwoWords_ReturnsWordsInReversedOrder()
+        {
+            ArrangeActAssert("hello world", "world hello");
+        }
+
+        [TestCase("hello glorious world", "world glorious hello")]
+        [TestCase("hello glorious world ohoy", "ohoy world glorious hello")]
+        public void Reverse_WithUnkownAmountOfWords_ReturnsWordsInReversedOrder(string given, string expected)
+        {
+            ArrangeActAssert(given, expected);
+        }
+
+        [TestCase("hello world!", "world hello!")]
+        [TestCase("how are you?", "you are how?")]
+        [TestCase("hello, world", "world, hello")]
+        public void Reverse_WithSpecialCharacters_ReturnsWordsInReversedOrderWithSpecialCharactersInPlace(string given, string expected)
+        {
+            ArrangeActAssert(given, expected);
+        }
+
+        private static void ArrangeActAssert(string given, string expected)
+        {
+            var wordReverser = new WordReverser();
+            string result = wordReverser.Reverse(given);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ FFCG.Reverser/FFCG.Reverser/WordReverser.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FFCG.Reverser
+{
+    public class WordReverser
+    {
+        private readonly string[] _specialCharacters = {".", ",", "!", "?"};
+
+        public string Reverse(string text)
+        {
+            string paddedText = Prepare(text);
+            var allWords = paddedText.Split(' ').ToList();
+            int totaltNumberOfWords = allWords.Count();
+
+            var specials = ExtractSpecialCharacters(allWords).ToList();
+            var onlyWords = RemoveSpecialCharacters(allWords).Reverse().ToList();
+
+            var result = new List<string>();
+
+            for (int index = 0; index < totaltNumberOfWords; index++)
+            {
+                string word = "";
+                if (onlyWords.Count() > index)
+                    word = onlyWords[index];
+
+                var specialCharacter = specials.FirstOrDefault(x => x.Index == index);
+                if(specialCharacter != null)
+                    result.Add(specialCharacter.Character);
+
+                result.Add(word);
+            }
+
+            var joinedString = string.Join(" ", result).Trim();
+
+            return Rebuild(joinedString);
+        }
+
+        private string Rebuild(string text)
+        {
+            foreach (var specialCharacter in _specialCharacters)
+            {
+                text = text.Replace(" " + specialCharacter, specialCharacter);
+            }
+
+            return text;
+        }
+
+        private IEnumerable<string> RemoveSpecialCharacters(List<string> allWords)
+        {
+            allWords.RemoveAll(x => _specialCharacters.Contains(x));
+
+            return allWords;
+        }
+
+        private IEnumerable<SpecialCharacter> ExtractSpecialCharacters(List<string> allWords)
+        {
+            for (int index = 0; index < allWords.Count; index++)
+            {
+                var word = allWords[index];
+                if(_specialCharacters.Contains(word))
+                    yield return new SpecialCharacter(index, word);
+            }
+        }
+
+        private string Prepare(string text)
+        {
+            foreach (var specialCharacter in _specialCharacters)
+            {
+                text = text.Replace(specialCharacter, " " + specialCharacter);
+            }
+
+            return text;
+        }
+    }
+
+    public class SpecialCharacter
+    {
+        public int Index { get; private set; }
+        public string Character { get; private set; }
+
+        public SpecialCharacter(int index, string character)
+        {
+            Index = index;
+            Character = character;
+        }
+    }
+}
