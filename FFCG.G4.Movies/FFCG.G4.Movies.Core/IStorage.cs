7b7330efbb31c9dcecde1b8115890517978a7b55
jesperbrannstrom <jesper.brannstrom@ffcg.se>
2016-02-10 21:03:05 +0100

    Implement simple json storage

---
+++ FFCG.G4.Movies/FFCG.G4.Movies.Core/IStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace FFCG.G4.Movies.Core
+{
+    public interface IStorage
+    {
+        IEnumerable<Movie> GetAll();
+        void Add(Movie movie);
+    }
+}
