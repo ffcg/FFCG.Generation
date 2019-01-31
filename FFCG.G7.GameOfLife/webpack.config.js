b5964536843a0bf5495a4736c7223115a90d8f57
Jesper Brännström <jesper@brickchain.com>
2019-01-31 15:31:05 +0100

    Add empty frontend for FFCG.G7.GameOfLife

---
+++ FFCG.G7.GameOfLife/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    web: './src/web/index.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  target: 'node'
+};
